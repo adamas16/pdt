64539eccfc740e36f9f41aa802045339e71f6ae8
adamas16 <adamas16@mail.ru>
2020-02-24 20:27:48 +0300

    1.6

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,7 +1,12 @@
 public class MyFirstProgram {
 
     public static void main(String[] args){
-        System.out.println("Hello world");
+        String somebody = "world";
+        System.out.println("Hello " + somebody + "!");
+
+        double l = 6.1;
+        double t = l * l;
+        System.out.println("Площадь квадрата со сторон l = " + l + " равна " + t);
     }
 
 }
