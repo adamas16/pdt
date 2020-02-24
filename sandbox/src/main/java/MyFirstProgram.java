d79ace8d498b8a33791f877433f76f7d4e17d6b0
adamas16 <adamas16@mail.ru>
2020-02-24 21:13:42 +0300

    1.9. Делаем из функции метод

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -6,20 +6,11 @@ public class MyFirstProgram {
         hello("me");
         hello("you");
 
-        System.out.println("Площадь квадрата со сторон l = " + s.l + " равна " + area(s));
-        System.out.println("Площадь прямоугольника со сторонами a = " + r.a + " и b = " + r.b + " равна " + area(r));
+        System.out.println("Площадь квадрата со сторон l = " + s.l + " равна " + s.area());
+        System.out.println("Площадь прямоугольника со сторонами a = " + r.a + " и b = " + r.b + " равна " + r.area());
     }
 
     public static void hello(String somebody){
         System.out.println("Hello " + somebody + "!");
     }
-
-    public static double area(Square s){
-        return s.l*s.l;
-    }
-
-    public static double area(Rectangle r){
-        return r.a*r.b;
-    }
-
 }
--- sandbox/src/main/java/Square.java
+++ sandbox/src/main/java/Square.java
@@ -5,4 +5,7 @@ public class Square {
         this.l = l;
     }
 
+    public double area(){
+        return this.l * this.l;
+    }
 }
