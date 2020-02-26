0e7b8ae6109b30ebf2defbea8d8b9a104292acff
adamas16 <adamas16@mail.ru>
2020-02-27 02:29:33 +0300

    Добавлен тест удаления контактов

--- addressbook-web-tests/src/test/java/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/GroupCreationTest.java
@@ -1,13 +1,4 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import java.util.concurrent.TimeUnit;
 
 
 public class GroupCreationTest extends TestBase{
