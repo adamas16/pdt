243132092460146b95f4234225f32f6270c2d46b
adamas16 <adamas16@mail.ru>
2020-03-02 23:13:53 +0300

    Задание 2(fix)

--- addressbook-web-tests/src/test/java/TestBase.java
+++ addressbook-web-tests/src/test/java/TestBase.java
@@ -4,10 +4,8 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 
 import java.util.concurrent.TimeUnit;
 
